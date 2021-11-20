4e1eaca4dd3289598fd1d114468ac557fae6c5e1
Anil Mallem <83568202+Anils117@users.noreply.github.com>
2021-11-20 23:27:14 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# set_terminal_lock
+# How to run this repo
+# Run these commands in Terminal
+# step-1: git clone https://github.com/Anils117/set_terminal_lock.git #cloning repo
+# step-2: cd set_terminal_lock #get into directory
+# step-3: chmod +x changepassword.sh #giving excutable permission
+# step-4: ./changepassword.sh #running file
+# step-5: exit #exit from terminal
+-----------------------------------------
+#to remove this lock setup
+
+# step-1: cd /home
+# step-2: remove setlock from .bashrc file.
---
+++ changepassword.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+password=$(zenity --password --title="Authentication") #Reading Password fron User
+echo
+sed "s/user/$password/g" lock.sh > setlock #Replacing current password in lock.sh file and putting it in setlock file.
+rm lock.sh #removing lock.sh file
+sudo chmod +x  setlock #giving excutable permission to setlock
+sudo mv setlock /usr/bin #moving setlock to /usr/bin Directory
+sudo echo setlock  >> $HOME/.bashrc #adding setlock file to .bashrc file in home to run start in terminal.
+#here bashrc file only runs the file present in /usr/bin.
+sudo rm -r ../set_terminal_lock_zenity #removing set_terminal lock directory.
